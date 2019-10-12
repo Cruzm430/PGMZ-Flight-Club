9968bbce820beca358e9ac65993d3035682d07c2
Zach Levine <zblevine@gmail.com>
2019-10-12 17:57:11 -0400

    server setup

--- server.js
+++ server.js
@@ -3,4 +3,11 @@ const app = express();
 app.use(express.json());
 const path = require('path');
 const db = require('./db/index');
-const { User, Shoe, Category }
+const { User, Shoe, Category } = db.models;
+
+const port = process.env.PORT || 3000;
+
+db.syncAndSeed()
+  .then(() => app.listen(port, ()=> console.log(`listening on port ${port}`)));
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
