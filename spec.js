7a82a408e270bb2e8d941f84dea88e4c7b2c4d5a
Zach Levine <zblevine@gmail.com>
2019-10-12 16:52:57 -0400

    first tests added

---
+++ spec.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai');
+const db = require('./db');
+const { User, Shoe, Category } = db.models;
+
+describe('Data Layer', () => {
+  beforeEach(() => db.syncAndSeed());
+  describe('is our stuff running', () => {
+    it('true is true', () => {
+      expect(true).to.equal(true);
+    })
+  })
+  describe('User model', () => {
+    it('Creates at least one user', async () => {
+      expect((await User.findAll()).length).to.not.be(0);
+    })
+    it('All users have name and email', async () => {
+      const users = await User.findAll();
+      expect(users.filter(user => user.email).length).to.equal(users.length);
+      expect(users.filter(user => user.name).length).to.equal(users.length);
+    })
+    it('Admin functionality works correctly', async () => {
+      const users = await User.findAll();
+      expect(users.find(user => user.name === 'Zach').admin).to.equal(true);
+      expect(users.find(user => user.name === 'Zach').admin).to.equal(false);
+    })
+  })
+})
+
+describe('Routes', () => {
+
+});
