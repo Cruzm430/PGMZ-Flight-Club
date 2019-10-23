089e085695687d367d3c774073dc302bd44df887
cruzm430 <cruzm430@gmail.com>
2019-10-23 10:42:01 -0400

    Home Cards.1

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,23 +6,6 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-import Login from './Login'
-import Welcome from './Welcome'
-
-// const Home = ({shoes, auth, logout})=>{
-//   return(
-//     <div>
-//       <h1>
-//         Check Our Inventory!
-//       </h1>
-//       {
-//         shoes.map(shoe=><div key={shoe.id}>
-//           <div><img src={shoe.imageURL}/></div>
-//         <Link to={`/product/${shoe.id}`}>{shoe.name}: ${shoe.price}</Link></div>)
-//       }
-//     </div>
-//   )
-// }
 
 const Home = ({shoes, auth, logout})=>{
   return(
@@ -52,24 +35,3 @@ const mapStateToProps = ({shoes}) =>{
 }
 
 export default connect(mapStateToProps)(Home)
-
-
-// const Home = ({shoes,auth,logout})=>{
-//   return(
-//     <div>
-//       <Grid container style={{padding:'100px', margin:'0'}}>
-//       {
-//         shoes.map(shoe=>
-//           <Grid item key={shoe.id} style={{padding:'100 px'}}>
-//             <Card>
-//               <CardMedia><img src={shoe.imageURL} height={'400 px'} width={'400 px'}/></CardMedia>
-//               <CardContent style={{background:'lightGray'}}>
-//                 <a href={`/shoes/${shoe.id}`} style={{textDecoration:'none', color:'white'}}><Typography>{shoe.name}: ${shoe.price}</Typography></a>
-//               </CardContent>
-//             </Card>
-//           </Grid>)
-//       }
-//     </Grid>
-//     </div>
-//   )
-// }
