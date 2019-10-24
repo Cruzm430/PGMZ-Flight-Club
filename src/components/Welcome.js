3f993fc767e05f28ef695e420c7ceebc08d69c2a
Zach Levine <zblevine@gmail.com>
2019-10-24 19:49:32 -0400

    link works

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {actions} from '../store' 
+import {actions} from '../store';
+import { Link } from 'react-router-dom';
 
 
 class Welcome extends React.Component {
